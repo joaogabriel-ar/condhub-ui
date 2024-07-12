b36c43e6746b245ce1ee90e7ce6068d24d6b8ed2
joaogabriel-ar <joaog151195@gmail.com>
2024-07-12 00:03:35 -0300

    First commit, first layout on the way; docker is ready

---
+++ src/pages/login/loginPage.tsx
@@ -0,0 +1,17 @@
+
+import "./loginPage.scss"
+import buildingsImage from "../../assets/images/buildings.png";
+
+export default function LoginPage () {
+    
+    return(
+        <div className="container">
+            <div className="form">
+                    
+            </div>
+            <div className="theme-image">
+                <img src={buildingsImage} alt="Buildings caricature" />
+            </div>
+        </div>
+    )
+}
